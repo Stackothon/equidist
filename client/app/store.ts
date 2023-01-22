1c52cef694622d1dbe87edaa46a1ad97d641a106
Edmund Fahrenheit <Fischerprogram@gmail.com>
2023-01-22 15:26:01 -0500

    Redux store functional for typescript. useAppDispatch and useAppSelector instead of useDispatch and useSelector from here on out. Search bar functional, connected to api/places. Slice created to display list of searched locations

--- client/app/store.ts
+++ client/app/store.ts
@@ -1,9 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
-// import logger from "redux-logger";
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import placesSlice from "../features/mapApp/mapAppSlice";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
-const store = configureStore({
-  reducer: {},
-  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+export const store = configureStore({
+  reducer: {
+    places: placesSlice,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
-export default store;
+// Use throughout app instead of plain `useDispatch` and `useSelector`
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+
+export type RootState = ReturnType<typeof store.getState>;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
